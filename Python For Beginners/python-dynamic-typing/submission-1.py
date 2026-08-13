c16e86b672a410ee311a40bf96eb946d5a66ce9b
Ashutosh Yadav <69635770+ash16-glitch@users.noreply.github.com>
2026-08-14 03:08:53 +0530

    Add: python-dynamic-typing - submission-1

---
+++ Python For Beginners/python-dynamic-typing/submission-1.py
@@ -0,0 +1,10 @@
+variable = 10
+print(type(variable))
+variable = 12.4
+print(type(variable))
+variable = False
+print(type(variable))
+variable = ('ASH')
+print(type(variable))
+variable = [1,2,3,4]
+print(type(variable))
